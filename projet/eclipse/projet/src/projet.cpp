29fc1db6421f371c50f0a2ab5c2c4837756bf4dd
gui45 <guillaume.d.marceau@gmail.com>
2018-10-25 21:41:08 -0400

    debut sfml et imgaes de base

--- projet/eclipse/projet/src/projet.cpp
+++ projet/eclipse/projet/src/projet.cpp
@@ -12,120 +12,146 @@
 #include "moyenne.h"
 #include "Sedan.h"
 #include "conio.h" 
+#include <vector>
 #include <fstream>
 #include <map>
+#include <SFML/Graphics.hpp>
 using namespace std;
 
 int main() {
-	courte* courtePiste = new courte();
-	moyenne* moyennePiste = new moyenne();
-	longue* longuePiste = new longue();
+	sf::RenderWindow window(sf::VideoMode(800, 500), "SFML works!");
+	sf::CircleShape shape(100.f);
+	sf::Sprite sprite;
+	shape.setFillColor(sf::Color::Green);
+	sf::Texture image;
+	image.loadFromFile("images/6362.png");
+	sprite.setTexture(image);
+	shape.setPosition(200, 200);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-	
+		window.clear();
+		window.draw(shape);
+		window.draw(sprite);
+		window.display();
 
-	cout << "veuiller choisir une piste, courte[1], moyenne[2], longue[3]"<<endl;
-	Piste* piste;
-	int choix;
-	cin >> choix;
-	switch (choix) {
-	case 1:
-		piste = courtePiste;
-		break;
-	case 2:
-		piste = moyennePiste;
-		break;
-	case 3:
-		piste = longuePiste;
-		break;
-	default:
-		piste = moyennePiste;
-		break;
-	}
-	system("cls");
-	piste->afficher();
-	cout << "veuiller choisir votre voiture avec un nombre entre 1 et 3"<<endl;
-	int voiture = 1;
-	cin >> voiture;
-	if (voiture > 3 || voiture < 1) voiture = 1;
+		courte* courtePiste = new courte();
+		moyenne* moyennePiste = new moyenne();
+		longue* longuePiste = new longue();
 
-	list<Voiture*> liste = piste->getListeVoiture();
-	list<Voiture*>::iterator iterateur = liste.begin();
 
-	bool quitter = false;
-	bool victoire = false;
-	while (!quitter) {
-		system("cls");
-		cout << "'q' pour quitter, 'd' pour avancer, 'a' pour reculer,'s' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, 'w' pour suavegarder la partie en cour"<<endl;
-		piste->afficher();
-		char commande = 'd';
-		cin >> commande;
-		int i = 1;
-		switch (commande) {
-		case 'q':
-			quitter = true;
+
+		cout << "veuiller choisir une piste, courte[1], moyenne[2], longue[3]" << endl;
+		Piste* piste;
+		int choix;
+		cin >> choix;
+		switch (choix) {
+		case 1:
+			piste = courtePiste;
 			break;
-		case 'd':
-			iterateur = liste.begin();
-			(*iterateur)->avancer();
-			iterateur++;
-			(*iterateur)->avancer();
-			iterateur++;
-			(*iterateur)->avancer();
+		case 2:
+			piste = moyennePiste;
 			break;
-		case 'a':
-			i = 1;
-			for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
-				if (i == voiture)
-					(*iterateur)->reculer();
-				else
-					(*iterateur)->avancer();
-				i++;
-			}
+		case 3:
+			piste = longuePiste;
 			break;
-		case 's':
-			i = 1;
-			for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
-				if (i == voiture)
-					(*iterateur)->avancer();
-				else
-					(*iterateur)->reculer();
-				i++;
-			}
-			break;
-		case 'n':
-			i = 1;
-			for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
-				if (i == voiture)
-					(*iterateur)->nitro();
-				else
-					(*iterateur)->avancer();
-				i++;
-			}
-			break;
-		case 'w':
-			ofstream fichier;
-			fichier.open("data/courses.xml");
-			fichier << piste->exporter();
-			fichier.close();
+		default:
+			piste = moyennePiste;
 			break;
 		}
-		i = 1;
-		for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
-			if ((*iterateur)->getDistanceParcourue() >= piste->getLongueur()) {
+		system("cls");
+		piste->afficher();
+		cout << "veuiller choisir votre voiture avec un nombre entre 1 et 3" << endl;
+		int voiture = 1;
+		cin >> voiture;
+		if (voiture > 3 || voiture < 1) voiture = 1;
+
+		list<Voiture*> liste = piste->getListeVoiture();
+		list<Voiture*>::iterator iterateur = liste.begin();
+
+		bool quitter = false;
+		bool victoire = false;
+		while (!quitter) {
+			system("cls");
+			cout << "'q' pour quitter, 'd' pour avancer, 'a' pour reculer,'s' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, 'w' pour suavegarder la partie en cour" << endl;
+			piste->afficher();
+			char commande = 'd';
+			cin >> commande;
+			int i = 1;
+			switch (commande) {
+			case 'q':
 				quitter = true;
-				if (i == voiture)
-					victoire = true;
+				break;
+			case 'd':
+				iterateur = liste.begin();
+				(*iterateur)->avancer();
+				iterateur++;
+				(*iterateur)->avancer();
+				iterateur++;
+				(*iterateur)->avancer();
+				break;
+			case 'a':
+				i = 1;
+				for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
+					if (i == voiture)
+						(*iterateur)->reculer();
+					else
+						(*iterateur)->avancer();
+					i++;
+				}
+				break;
+			case 's':
+				i = 1;
+				for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
+					if (i == voiture)
+						(*iterateur)->avancer();
+					else
+						(*iterateur)->reculer();
+					i++;
+				}
+				break;
+			case 'n':
+				i = 1;
+				for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
+					if (i == voiture)
+						(*iterateur)->nitro();
+					else
+						(*iterateur)->avancer();
+					i++;
+				}
+				break;
+			case 'w':
+				ofstream fichier;
+				fichier.open("data/courses.xml");
+				fichier << piste->exporter();
+				fichier.close();
 				break;
 			}
-			i++;
+			i = 1;
+			for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
+				if ((*iterateur)->getDistanceParcourue() >= piste->getLongueur()) {
+					quitter = true;
+					if (i == voiture)
+						victoire = true;
+					break;
+				}
+				i++;
+			}
 		}
-	}
-	system("cls");
-	if (victoire)
-		cout << "Felicitation!!! tu a gagner";
-	else
-		cout << "haha ta perdu !";
+		system("cls");
+		if (victoire)
+			cout << "Felicitation!!! tu a gagner";
+		else
+			cout << "haha ta perdu !";
 
-	_getch();
+		_getch();
+
+	}
 	return 0;
 }
