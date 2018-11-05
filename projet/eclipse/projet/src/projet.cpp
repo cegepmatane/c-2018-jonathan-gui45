df40b1769c85bc4bd098f500c38ecf3400c8314c
gui45 <guillaume.d.marceau@gmail.com>
2018-11-05 09:33:02 -0500

    corrections mineurs

--- projet/eclipse/projet/src/projet.cpp
+++ projet/eclipse/projet/src/projet.cpp
@@ -33,7 +33,7 @@ int main() {
 	//cout << "veuiller choisir une piste, courte[1], moyenne[2], longue[3]" << endl;
 	sf::Font font;
 	font.loadFromFile("fonts/arial.ttf");
-	sf::Text Toptions1("veuiller choisir une piste, courte[1], moyenne[2], longue[3]", font, 30);
+	sf::Text Toptions1("veuiller choisir une piste, courte[1], moyenne[2], longue[3]\n appuyer sur entrer apres chaque commande \n appuyer sur entree apres chaque commande", font, 30);
 	Toptions1.move(0, 300);
 	Toptions1.setFillColor(sf::Color::Black);
 	window.clear();
@@ -60,9 +60,9 @@ int main() {
 	}
 	system("cls");
 	//piste->afficher();
-	//cout << "veuiller choisir votre voiture avec un nombre entre 1 et 3" << endl;
 	list<sf::Sprite*> listeSpriteVoitures = piste->afficherImages();
-	sf::Text Toptions2(piste->afficher() + "\nveuiller choisir votre voiture avec un nombre entre 1 et 3", font, 30);
+	sf::Text Toptions2(piste->afficher() + "\nveuiller choisir votre voiture avec un nombre entre 1 et 3\n appuyer sur entrer apres chaque commande \n appuyer sur entree apres chaque commande", font, 30);
+	cout << "veuiller choisir votre voiture avec un nombre entre 1 et 3" << endl;
 	Toptions2.move(0, 300);
 	Toptions2.setFillColor(sf::Color::Black);
 	window.clear();
@@ -89,11 +89,11 @@ int main() {
 	{
 		//while (!quitter) {
 			system("cls");
-			cout << "'q' pour quitter, 'd' pour avancer, 'a' pour reculer,'s' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, 'w' pour suavegarder la partie en cour, ' ' pour changer de voiture" << endl;
+			cout << "'q' pour quitter, 'd' pour avancer, 'a' pour reculer,'s' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, 'w' pour suavegarder la partie en cour, ' ' pour changer de voiture, appuyer sur entrer apres chaque commande appuyer sur entree apres chaque commande" << endl;
 			//piste->afficher();
 
 			list<sf::Sprite*> listeSpriteVoitures = piste->afficherImages();
-			sf::Text Toptions2(piste->afficher() + "\n voiture selectionner : " + to_string(voiture) + "\n'q' pour quitter, 'd' pour avancer, 'a' pour reculer,\n's' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, \n'w' pour suavegarder la partie en cour, ' ' pour changer de voiture", font, 30);
+			sf::Text Toptions2(piste->afficher() + "\n voiture selectionner : " + to_string(voiture) + "\n'q' pour quitter, 'd' pour avancer, 'a' pour reculer,\n's' pour faire reculer tous sauf toi, 'n' pour beneficier de vitesse suplementaire, \n'w' pour suavegarder la partie en cour, 'e' pour changer de voiture \n appuyer sur entrer apres chaque commande \n appuyer sur entree apres chaque commande", font, 30);
 			Toptions2.move(0, 300);
 			Toptions2.setFillColor(sf::Color::Black);
 			window.clear();
@@ -159,12 +159,11 @@ int main() {
 				fichier << piste->exporter();
 				fichier.close();
 				break;
-			case ' ':
+			case 'e':
 				voiture++;
 				if (voiture > 3) voiture = 1;
 				break;
 			}
-			
 
 			i = 1;
 			for (iterateur = liste.begin(); iterateur != liste.end(); iterateur++) {
